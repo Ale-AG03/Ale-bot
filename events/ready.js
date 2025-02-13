e58e045e46e81251ddf9d1bdc37f8712c1fac2f9
AleGonzaF03 <albertodeorozcof03@icloud.com>
2025-02-13 11:39:48 -0600

    comente el token de discord 3

--- events/ready.js
+++ events/ready.js
@@ -18,32 +18,3 @@ module.exports = async(client) => {
         status: "online", // Cambiar a idle o dnd si es necesario
     });
 }
-
-
-// const {Client, Events} = require("discord.js");
-
-// const fs = require("fs");
-// const path = require("path");
-
-// //crear nuevo cliente de discord
-// const client = new Client({
-//     intents: 53608447
-// });
-
-// fs.readdirSync("./events")
-// .filter((filename) => filename.endsWith(".js"))
-// .forEach((filename) => {
-//     try{
-//         const listener = require(`./events/${filename}`);
-//         const eventName = path.basename(filename, ".js");
-
-//         client.on(eventName, listener);
-//     }catch (err){
-//         console.log("[err] Ha ocurrido un error al cargar un evento", err)
-//     }
-// });
-
-// //conctar cliente con la aplicacion de discord
-// client.login(
-//     "MTMwNDgzODI1MjYxNTgyNzU4Ng.G6Mnvv.EUN1hgqiDCw1OqGWuewVpgaek_WWtOLwAUzcAs"
-// );
